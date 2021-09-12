d308dff3144bcdc3b6ec009c7d824bd9d86d392f
Andrew Smith <asmith@ves.org>
2021-09-12 15:25:36 -0400

    Initial Commit

---
+++ QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,25 @@
+// created by Andrew Smith
+
+
+
+
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        let lcquestion = question.lowercased()
+        if lcquestion.hasPrefix("hello") {
+        return "Why, hello there. How are you?"
+    }
+        if lcquestion.hasPrefix("good") {
+        return "I'm glad to hear that."
+    }
+        if lcquestion.hasPrefix("bad") {
+        return "I'm sorry to hear that. I hope the rest of you day is better."
+    }
+        if lcquestion.hasPrefix("ok") {
+        return "I hope the rest of you day is better."
+    }
+        else {
+        return "I'm sorry, I don't understand the question"
+        }
+    }
+}
